687edf2ea7e21bf5afe9caa152cefb0580495beb
TMSulik <shulich@hotmail.com>
2019-04-23 17:34:52 -0700

    Heroku link is in the README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# NYT-Mongo-Scraper
+# Mongo Scraper
 This is a browser-based interface that allows users to scrape articles from the New York Times, attach personal annotations to these articles, and save them to a local Mongo database.
+
+## Deployed on Heroku
+https://immense-ridge-60214.herokuapp.com/
 
--- server.js
+++ server.js
@@ -30,6 +30,13 @@ app.use(router);
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// const databaseUri = "mongodb://localhost/mongoHeadlines";
+// if(process.env.MONGODB_URI) {
+//     mongoose.connect(process.env.MONGODB_URI);    
+// } else {
+//     mongoose.connect(databaseUri);
+// }
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
